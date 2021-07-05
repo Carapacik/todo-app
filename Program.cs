a27f9998995407dc57f5942a98b5938350ad94cc
Roman <carapacik@gmail.com>
2021-07-05 20:34:05 +0300

    Add sln

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace todo_list
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # todo-list
+
 Simple ToDo list on Angular
